8bebcdf8e5d187db378a0a7ffa610b0435917ceb
onethps <onethps@gmail.com>
2022-06-07 01:57:13 +0300

    Updates

--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Nullable } from './Nullable';
-export type { ReturnComponentType } from './ReturnComponentType';
-export type { profileType } from './profile';
-export type { photosType } from './profile';
