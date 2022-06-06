c9a044ee196d99f3bc31996cb57cb921afa85299
onethps <onethps@gmail.com>
2022-06-07 01:57:13 +0300

    Updates

--- src/enums/PostsQuerys.ts
+++
@@ -1,4 +0,0 @@
-export enum POST_QUERYS {
-  CURRENT_POSTS_PAGE = 1,
-  LIMIT_POSTS_PER_PAGE = 2,
-}
--- src/enums/index.ts
+++
@@ -1,3 +0,0 @@
-export { ResponseCode } from './ResponseCode';
-export { SUGGEST_USERS_CONSTS, MY_FOLLOWS_CONSTS } from './UsersRequestCounts';
-export { POST_QUERYS } from './PostsQuerys';
